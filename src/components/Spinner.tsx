355d0b9aaeb318e5c8b962a7a7de00eab972f838
luanlcampos <luanlcampos15@gmail.com>
2023-02-22 10:18:06 -0800

    fixed loading icon size

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
     <svg
-      className="animate-spin -ml-1 mr-3 h-5 w-5"
+      className="animate-spin mb-5 h-10 w-10"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -12,7 +12,7 @@ export default function Spinner() {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
